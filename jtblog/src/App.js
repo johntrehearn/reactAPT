7cf022f47c61b27e1ff22702ae26b1ea175c02da
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 11:10:30 +0300

    notes added

--- jtblog/src/App.js
+++ jtblog/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  // when we use {} React knows we want to use JavaScript / output a dynamic variable.
+
+  const title = 'Welcome to the new blog';
+
+  const likes = 50;
+
+  // React converts to a string when rendering to the DOM.
+
+  // We can use JavaScript expressions inside JSX by wrapping them in curly braces.
+
+  // The only thing we CANNOT output is booleans or objects.
+
   return (
     <div className="App">
       <div className="content">
         <h1>App Component</h1>
+        <h2>{title}</h2>
+        <p>Liked {likes} times</p>
       </div>
     </div>
   );
