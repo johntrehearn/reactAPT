7658bc64ce5734f4cc390d28e2863dd092a19ad3
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 14:11:15 +0300

    links moved to right

--- jtblog/src/Navbar.js
+++ jtblog/src/Navbar.js
@@ -2,17 +2,19 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The JT-Blog</h1>
-      <a href="/">Home</a>
-      <a
-        href="/create"
-        style={{
-          color: 'white',
-          backgroundColor: '#f1356d',
-          borderRadius: '8px',
-        }}
-      >
-        New Blog
-      </a>
+      <div className="alinks">
+        <a href="/">Home</a>
+        <a
+          href="/create"
+          style={{
+            color: 'white',
+            backgroundColor: '#f1356d',
+            borderRadius: '8px',
+          }}
+        >
+          New Blog
+        </a>
+      </div>
     </nav>
   );
 };
