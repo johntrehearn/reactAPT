a55cdeb52ab0024bb9dccd8b6aaea48acd58f61d
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 14:24:21 +0300

    second one with annon function

--- jtblog/src/Home.js
+++ jtblog/src/Home.js
@@ -2,11 +2,22 @@ const handleClick = () => {
   console.log('Button clicked');
 };
 
+const handleClickAgain = (name) => {
+  console.log('Hello ' + name);
+};
+
 const Home = () => {
   return (
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          console.log('Hello');
+        }}
+      >
+        Click Me Again
+      </button>
     </div>
   );
 };
