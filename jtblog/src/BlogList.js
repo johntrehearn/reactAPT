130e0fb13954f8bc4fa81073ac6773b9c6bd5a87
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 16:08:32 +0300

    destructuring props

---
+++ jtblog/src/BlogList.js
@@ -0,0 +1,32 @@
+// here in BlogList.js we need to receive the prop that we passed from Home.js
+
+// we use props as an argument to the BlogList function i.e. const BlogList = (props) => { ... }
+
+// here we get access to an argument inside the component called props
+
+//Props is just an an object that contains all the properties that were passed to a component
+
+// we can access the blogs array by using const blogs = props.blogs;
+
+// if we log it to the console we can see the array of blogs
+
+const BlogList = (props) => {
+  const blogs = props.blogs;
+  const title = props.title;
+
+  console.log(props, blogs);
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
--- jtblog/src/Home.js
+++ jtblog/src/Home.js
@@ -1,4 +1,27 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
+
+// to give BlogList.js access to the blogs we could redefine the blogs in the BlogsList component OR we could pass it as a prop to BlogList.js
+
+// props is a way to pass data from a parent component to a child component
+
+// props is an object that contains all the properties that were passed to a component
+
+// props is short for properties
+
+// props blogs={blogs}
+
+// This is passing the blogs array from the Home component to the BlogList component
+
+// In BlogList.js we need to receive the prop that we passed from Home.js
+
+// we use props as an argument to the BlogList function i.e. const BlogList = (props) => { ... }
+
+// Then in BlogList.js we get access to an argument inside the component called props
+
+//Props is just an an object that contains all the properties that were passed to a component
+
+// so in BlogList.js we can access the blogs array by using const blogs = props.blogs;
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -9,13 +32,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      <h2>Blogs</h2>
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 };
