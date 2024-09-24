43c52bf1d6bbae8881f8b1b9c48ee24522c99a15
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 11:13:07 +0300

    notes on brackets

--- jtblog/src/App.js
+++ jtblog/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   // React converts to a string when rendering to the DOM.
 
-  // We can use JavaScript expressions inside JSX by wrapping them in curly braces.
+  // We can use JavaScript expressions inside JSX by wrapping them in curly braces. i.e. A javascripts statment that outputs a value. Either a string or number or an array
 
   // The only thing we CANNOT output is booleans or objects.
 
@@ -19,6 +19,9 @@ function App() {
         <h1>App Component</h1>
         <h2>{title}</h2>
         <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>{'Hello'}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
       </div>
     </div>
   );
