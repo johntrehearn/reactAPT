99938293436148f1de05e17a78e4e366826d7fb7
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 21:15:10 +0300

    updated notes

--- jtblog/src/Home.js
+++ jtblog/src/Home.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 // to give BlogList.js access to the blogs we could redefine the blogs in the BlogsList component OR we could pass it as a prop to BlogList.js
@@ -40,6 +40,21 @@ const Home = () => {
   };
 
   // handleDelete is defined in here as the data is initialised in here and this is where the state is. So we want to use the setBlog method here component to edit the state and THEN PASS THIS THROUGH AS A PROP TO THE BLOGLIST COMPONENT. i.e. pass handleDelete through as a prop to the BlogList component as you can pass functions as props handleDetete={handleDelete} is passing the prop through to the BlogList.js componenent it need to be accepted in the destructed top of const BlogList = ({blogs, title, handleDelete}) => { ... } so we can use it.
+
+  // useEffect runs a function every render of the component. So once when the component is first loaded and then again every time the component is re-rendered. ie. when the state changes - useState is called. IT IS A WAY TO RUN CODE ON EVERY RENDER.
+
+  // useEffect is not stored in a constant as it does not return anything to us.
+  // All we need to do is pass as an argument a fiunction that we want to run on every render of the component.
+  // normally we use it to fetch data or communicate with some sort of authentication service (these are known as side effects in react)
+
+  // useEffect takes a second argument which is an array of dependencies. This is an array of values that the effect depends on. If any of these values change the effect will run again. If we pass an empty array it will only run on the first render of the component. If we pass nothing it will run on every render of the component.
+
+  // ONE THING TO BE CAREFUL OF is changing the state inside of a useEffect. If we do this it will cause an infinite loop. This is because changing the state causes a re-render and useEffect runs on every render. So if we change the state inside of useEffect it will cause a re-render and useEffect will run again and so on. This is why we pass an empty array as the second argument to useEffect. This will only run the effect on the first render of the component.
+
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(blogs);
+  }, [blogs]);
 
   return (
     <div className="home">
