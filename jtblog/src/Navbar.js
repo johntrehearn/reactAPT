6df6b12ef10226307ecbeb34904f5b9cb1f04417
johntrehearn <johntrehearnbc@gmail.com>
2024-09-24 11:35:29 +0300

    Inline styles in react

--- jtblog/src/Navbar.js
+++ jtblog/src/Navbar.js
@@ -3,7 +3,16 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>The JT-Blog</h1>
       <a href="/">Home</a>
-      <a href="/create">New Blog</a>
+      <a
+        href="/create"
+        style={{
+          color: 'white',
+          backgroundColor: '#f1356d',
+          borderRadius: '8px',
+        }}
+      >
+        New Blog
+      </a>
     </nav>
   );
 };
